ea402feaa3e8fc431a763ee65cced6855563e025
hax-rockvan <hax.rockvan@gmail.com>
2021-05-21 16:30:25 +0800

    test update

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,21 +1,21 @@
 // Request form modal close and toast alert
-document.getElementById('requestForm').addEventListener('submit', function () {
-	document.getElementById('bookTittleInput').value = '';
-	document.getElementById('authorNameInput').value = '';
-	document.getElementById('emailInput').value = '';
-	document.getElementById('cancelRequest').click();
+document.getElementById("requestForm").addEventListener("submit", function () {
+	document.getElementById("bookTittleInput").value = "";
+	document.getElementById("authorNameInput").value = "";
+	document.getElementById("emailInput").value = "";
+	document.getElementById("cancelRequest").click();
 
-	var myAlert = document.getElementById('bookRequestToast'); //select id of toast
+	var myAlert = document.getElementById("bookRequestToast"); //select id of toast
 	var bsAlert = new bootstrap.Toast(myAlert); //inizialize it
 	bsAlert.show(); //show it
 });
 
 // Subscriber Toast Alert
 document
-	.getElementById('subscriberForm')
-	.addEventListener('submit', function () {
-		document.getElementById('subscriberInput').value = '';
-		var myAlert = document.getElementById('newsletterToast'); //select id of toast
+	.getElementById("subscriberForm")
+	.addEventListener("submit", function () {
+		document.getElementById("subscriberInput").value = "";
+		var myAlert = document.getElementById("newsletterToast"); //select id of toast
 		var bsAlert = new bootstrap.Toast(myAlert); //inizialize it
 		bsAlert.show(); //show it
 	});
